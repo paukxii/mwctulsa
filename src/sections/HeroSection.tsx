0b25f18417b1e8bacbab622c6ef749168fc19bea
PX <coderpauk@gmail.com>
2024-03-20 13:19:44 -0500

    3/20/2024

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -5,15 +5,15 @@ export default function HeroSection() {
   return (
     <SectionContainer
       id='hero'
-      bgImage='bg-hero h-[50rem] sm:h-[39rem] md:h-[56rem] mb-36 sm:mb-24 md:mb-[9rem] w-full'
+      bgImage='bg-hero h-[54rem] sm:h-[40rem] md:h-[56rem] mb-36 sm:mb-24 md:mb-[9rem] w-full'
       hero={true} >
-      <div className='relative w-full'>
+     <div className='relative w-full'>
         <div className='grid grid-cols-1 h-screen w-full place-items-center sm:h-[750px] text-center'>
           <div className='text-center hidden'>
             <h1 className='text-white text-5xl md:text-[80px] leading-none text-shadow'> Welcome to <br/> MWC </h1>
             <p className='text-xl md:text-3xl text-white text-shadow'>မြန်မာတို့ စုဝေးကိုးကွယ်ရာနေရာမှ<br/>ကြိုဆိုပါ၏။</p>
           </div>
-          <div className='absolute bottom-5 sm:bottom-16 sm:w-full lg:w-[65%]'>
+         <div className='absolute bottom-5 sm:bottom-16 sm:w-full lg:w-[65%]'>
               <Event_slider />
           </div>
         </div>
