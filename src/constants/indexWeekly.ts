09b07ea936289f9946af91e175ce04af0f5b92f0
Pauk <coderpauk@gmail.com>
2024-06-27 21:17:30 -0500

    Week of 6/30/2024

--- src/constants/indexWeekly.ts
+++ src/constants/indexWeekly.ts
@@ -49,7 +49,7 @@ export const our_shcedules = [
 export const upcoming_events = [
     {
         name: 'Weekly',
-        image: '/imgs/weekly/Weekly06-23-2024.jpg',
+        image: '/imgs/weekly/Weekly06-30-24.jpg',
     },
     // {
     //     name: 'Weekly',
