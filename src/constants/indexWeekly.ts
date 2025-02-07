458af8a60fdb5e3fd1b96cf5e42002eeaec10954
Pauk <coderpauk@gmail.com>
2025-02-07 10:54:05 -0600

    Week of 02-09-2025

--- src/constants/indexWeekly.ts
+++ src/constants/indexWeekly.ts
@@ -49,7 +49,7 @@ export const our_shcedules = [
 export const upcoming_events = [
     {
         name: 'Weekly',
-        image: '/imgs/weekly/Weekly-02-02-2025.jpg',
+        image: '/imgs/weekly/Weekly-02-09-2025.jpg',
     },
     // {
     //     name: 'Weekly',
